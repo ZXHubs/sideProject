0e9ea618231c8885609116807cac781eeeab18a1
ZhangX <zxjerry@163.com>
2022-02-21 22:15:05 +0800

    关注、点赞按钮

--- DuoYinDemo/DuoYinDemo/Home/Models/FeedModel.swift
+++ DuoYinDemo/DuoYinDemo/Home/Models/FeedModel.swift
@@ -12,7 +12,7 @@ struct FeedModel {
     struct Author {
         var id: String?
         var name: String?
-        var avatarURL: URL?
+        var avatarURL: URL? = URL(string: "https://media.dayoftheshirt.com/images/shirts/it8r2/teepublic_hello-lisa-rainbow-apple-teepublic_1557204000.large.png")
     }
     
     var id: String?

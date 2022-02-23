8fd764863abc01de39d1da9d4a8d6ed02d8e1ff4
ZhangX <zxjerry@163.com>
2022-02-23 15:34:09 +0800

    新建一个练习小项目

---
+++ exercise/exercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  exercise
+//
+//  Created by ZhangX on 2022/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

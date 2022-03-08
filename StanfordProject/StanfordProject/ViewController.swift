83c2a31ca75c8c1ec434cc6b0b0712a281cfa12c
ZhangX <zxjerry@163.com>
2022-03-08 20:01:20 +0800

    first

--- StanfordProject/StanfordProject/ViewController.swift
+++ StanfordProject/StanfordProject/ViewController.swift
@@ -9,6 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func touchCard(_ sender: UIButton) {
+        filpCard(withEmoji: "🥳", on: sender)
+    }
+    
+    @IBAction func secondCard(_ sender: UIButton) {
+        filpCard(withEmoji: "🤣", on: sender)
+    }
+    func filpCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = UIColor.white
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = UIColor.orange
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

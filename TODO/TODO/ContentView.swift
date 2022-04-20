1152132eb0be77b6eb58cfb1fdb9e0fe2e856304
ZXHubs <53455473+ZXHubs@users.noreply.github.com>
2022-04-20 10:40:34 +0800

    Add files via upload

---
+++ TODO/TODO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TODO
+//
+//  Created by ZhangX on 2022/4/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TODO/TODO/TODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOApp.swift
+//  TODO
+//
+//  Created by ZhangX on 2022/4/20.
+//
+
+import SwiftUI
+
+@main
+struct TODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
